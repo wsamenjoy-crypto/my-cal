aeeed259c9015067ad71ed3f9573ef9db1a900d9
wsamenjoy-crypto <wsamenjoy@gmail.com>
2026-01-02 22:15:45 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'calc-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/mathjs/11.8.0/math.js'
+];
+
+// تثبيت الخدمة وتخزين الملفات
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+// جلب الملفات من التخزين المؤقت عند غياب الإنترنت
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
